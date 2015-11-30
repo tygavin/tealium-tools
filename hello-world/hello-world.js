18ea3464ae5fdfeda3d89ffa8329ec3d99c15eea
Ty Gavin <ty@tealium.com>
2015-11-30 14:11:50 -0800

    initial add of Hello World .js and .html files

---
+++ hello-world/hello-world.js
@@ -0,0 +1,41 @@
+/* Tealium Tools Hello World Example 
+*
+* Github: https://github.com/tygavin/tealium-tools/ 
+* NOTE: This file may run multiple times on the same page.  Preserves current state if Tealium Tool is closed/opened.
+*
+*/
+
+var helloWorld = helloWorld || {
+  message : {},
+
+  // incoming "data" object from Tealium tools .html (Handlebars) UI
+  main : function( data ) {
+ 
+    var message = helloWorld.message;
+
+    switch ( data.command ){
+      case "next":
+        message.ui_finish = true;
+        message.ui_start = false;
+        message.data.text_area = data.text_area;
+        break;
+      case "restart":
+        message.ui_finish = false;
+        message.ui_start = true;
+        break;
+      default:
+        console.log( "Restart" );
+        message.data = {};
+        message.data.web_page_url = document.URL;
+        break;
+    }
+
+    tealiumTools.send( message );
+
+  }
+}
+
+window.helloWorld = helloWorld.main;
+
+tealiumTools.send( helloWorld.message );
+
