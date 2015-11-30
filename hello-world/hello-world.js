5da79941e0af85230961c0f4e1647cfba646e78a
Ty Gavin <ty@tealium.com>
2015-11-30 15:04:19 -0800

    fix for starting out in the right state

--- hello-world/hello-world.js
+++ hello-world/hello-world.js
@@ -6,28 +6,35 @@
 */
 
 var helloWorld = helloWorld || {
-  message : {},
+  
+  // Initialize message object to starting state for Tealium Tool
+  message : {
+    ui_start     : true,
+    data         : {
+      web_page_url : document.URL
+    }
+  },
 
-  // incoming "data" object from Tealium tools .html (Handlebars) UI
+  // Incoming "data" object from Tealium tools .html (Handlebars) UI
   main : function( data ) {
  
     var message = helloWorld.message;
 
     switch ( data.command ){
       case "next":
+        console.log( "Hello World: Next" );
         message.ui_finish = true;
         message.ui_start = false;
         message.data.text_area = data.text_area;
         break;
       case "restart":
+        console.log( "Hello World: Restart" );
         message.ui_finish = false;
         message.ui_start = true;
+        message.data.web_page_url = document.URL;
         break;
       default:
-        console.log( "Restart" );
-        message.data = {};
-        message.ui_start = true;
-        message.data.web_page_url = document.URL;
+        console.log( "Hello World: Unknown command received from Tealium Tool" );
         break;
     }
 
