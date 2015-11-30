0261c1242a3fecde3f54b136050cb120f74aaf8d
Ty Gavin <ty@tealium.com>
2015-11-30 15:41:01 -0800

    refactor

--- hello-world/hello-world.js
+++ hello-world/hello-world.js
@@ -7,31 +7,28 @@
 
 var helloWorld = helloWorld || {
   
-  // Initialize message object to starting state for Tealium Tool
-  message : {
-    ui_start     : true,
-    data         : {
-      web_page_url : document.URL
-    }
-  },
+  initialized : false,
 
-  // Incoming "data" object from Tealium tools .html (Handlebars) UI
-  main : function( data ) {
+  // Initialize message object with data object for passing information
+  message : { data : {} },
+
+  // Incoming tool object from Tealium tools .html (Handlebars) UI
+  main : function( tool ) {
  
     var message = helloWorld.message;
 
-    switch ( data.command ){
+    switch ( tool.command ){
+      case "start":
+        console.log( "Hello World: Start" );
+        message.ui_finish = false;
+        message.ui_start = true;
+        message.data.web_page_url = document.URL;
+        break;
       case "next":
         console.log( "Hello World: Next" );
         message.ui_finish = true;
         message.ui_start = false;
-        message.data.text_area = data.text_area;
-        break;
-      case "restart":
-        console.log( "Hello World: Restart" );
-        message.ui_finish = false;
-        message.ui_start = true;
-        message.data.web_page_url = document.URL;
+        message.data.text_area = tool.text_area;
         break;
       default:
         console.log( "Hello World: Unknown command received from Tealium Tool" );
@@ -45,5 +42,12 @@ var helloWorld = helloWorld || {
 
 window.helloWorld_main = helloWorld.main;
 
-tealiumTools.send( helloWorld.message );
+if ( !helloWorld.initialized ) { 
+  helloWorld.initialized = true; 
+  console.log( "Hello World: Initialized" );
+  helloWorld.main( { command : "start" } );
+}else{
+  console.log( "Hello World: /hello_world.js reloaded" );
+}
+
 
